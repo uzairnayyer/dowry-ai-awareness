1aaf5f5c3121d239ea3bbfa675aaad271bb513de
Uzair Nayyer <uzairnayyerw@gmail.com>
2026-02-20 18:03:36 +0500

    Modify app to use environment variable for port
    Updated app to run on dynamic port from environment variable.

--- app.py
+++ app.py
@@ -59,5 +59,6 @@ def record_pledge():
         'message': 'Thank you for taking a stand'
     })
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == '__main__':    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
